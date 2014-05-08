3b8b287778ad2471839fa61f02e21fc7472a70d7
bxie <bxie@MIT.edu>
2014-05-08 13:21:07 -0400

    energy changes with moves

--- DotProductive/v1.js
+++ DotProductive/v1.js
@@ -1,42 +1,47 @@
 function init(){
-	var stage, red1,red2, blk1, blk2, dragger, oldX, oldY;
-
-	var height = 500;
-	var width = 1000;
+	var MAX_ENERGY = 100.0;
+	var TOTAL_TIME = 100.0;
 
     var stage = new createjs.Stage("demoCanvas");
 	var dragger = new createjs.Container();
-	var total_time = 100.0;  //set to max time of game, in seconds
-	var	start_time = getTimeInSec();
-	var score = 0
-	var energy = 100
+	var gameTime = TOTAL_TIME;  //set to max time of game, in seconds
+	var	prev_time = getTimeInSec(); //time of previous tick
+	var score = 0;
+	var energy = MAX_ENERGY;
+	var energy_multiplier = 1.0; //At each second, energy drops this amount
+	var energy_dist_multiplier = 0.05; //for calculating energy loss from travel distance
 
-	//Time
-	timeLabel = stage.addChild(new createjs.Text("Time: "+start_time, "14px monospace", "#000"));
+	//Time Label
+	timeLabel = stage.addChild(new createjs.Text("Time: "+prev_time, "14px monospace", "#000"));
 	timeLabel.lineHeight = 15;
 	timeLabel.textBaseline = "top"
 	timeLabel.x = 10;
 	timeLabel.y = 10;
 
-	//Energy
+	//Energy Label
 	energyLabel = stage.addChild(new createjs.Text("Energy: "+energy, "14px monospace", "#000"));
 	energyLabel.lineHeight = 15;
 	energyLabel.textBaseline = "top"
 	energyLabel.x = 180;
 	energyLabel.y = 10;
 
-	//Score
+	//Score Label
 	scoreLabel = stage.addChild(new createjs.Text("Score: "+score, "14px monospace", "#000"));
 	scoreLabel.lineHeight = 15;
 	scoreLabel.textBaseline = "top"
 	scoreLabel.x = 350;
 	scoreLabel.y = 10;
 
-	//updates time
+	//updates time, energy
 	createjs.Ticker.on("tick", tick);
 	function tick(event) {
-		var delta = getTimeInSec() - start_time;
-		timeLabel.text = "Time Left: "+ Math.round(total_time - delta);
+		var delta = getTimeInSec() - prev_time;
+		prev_time = getTimeInSec();
+		gameTime = gameTime - delta;
+		energy = (energy - delta) * energy_multiplier;
+		timeLabel.text = "Time Left: "+ Math.round(gameTime);
+		energyLabel.text = "Energy: "+Math.round(energy);
+
 		stage.update(event);
 	};
 
@@ -66,7 +71,7 @@ function init(){
 	stage.mouseMoveOutside = true;
 
 	//Sprite we control
-	var main = createCircle("blue", 30, 40, height/2);
+	var main = createCircle("blue", 30, 40, stage.canvas.height/2);
 	stage.addChild(main);
 
 	//main follows mouse
@@ -78,13 +83,25 @@ function init(){
 
     stage.update();
 
+    //makes a shape clickable
     function createClickable(shape){
 		var dragger = new createjs.Container();
 		dragger.addChild(shape);
 
 		dragger.on("click",function(evt){
+			var oldX = main.x;
+			var oldY = main.y;
 			main.x = evt.stageX;
 			main.y = evt.stageY;
+
+			console.log(calcDistance(oldX, oldY, main.x, main.y));
+
+			//calculating and updating energy loss from travelling
+			dist = calcDistance(oldX, oldY, main.x, main.y);
+			e_delta = -1.0 * calc_travel_energy(dist);
+			console.log(e_delta);
+			updateEnergy(e_delta);
+
 			target = dragger.getObjectUnderPoint();
 			if(target.timeAlive<0.0001){
 				//productive object
@@ -117,10 +134,20 @@ function init(){
 		stage.update();
 	}
 
+	//adds 1 to score and updates label
 	function incrementScore(){
 		score = score + 1;
 		scoreLabel.text = "Score: "+ score;
 		stage.update();
+	}
+
+	//Calculates energy loss
+	//Energy Loss = (dist)*(energy_dist_multiplier)*(tiredness)
+	//tiredness = log(1+MAX_ENERGY-energy)+1
+	function calc_travel_energy(dist){
+		tiredness = Math.log(1+ (MAX_ENERGY-energy)/MAX_ENERGY)+1; 
+		console.log(tiredness);
+		return dist * energy_dist_multiplier*tiredness;
 	}
 };
 
@@ -154,6 +181,11 @@ function createCircle(color, size, xPos, yPos, timeAlive, isProductive){
 function getTimeInSec(){
 	return createjs.Ticker.getTime()/1000
 };
+
+//Distance between two coordinates
+function calcDistance(startX, startY, endX, endY){
+	return Math.sqrt( Math.pow(startX-endX,2) + Math.pow(startY-endY,2) );
+}
 
 /**
 Next Steps:
