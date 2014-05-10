704fe01fa4a2095a3334661e7df64915b48735e6
bxie <bxie@MIT.edu>
2014-05-10 11:04:05 -0400

    play test for dots

--- DotProductive/v1.js
+++ DotProductive/v1.js
@@ -7,7 +7,8 @@ function init(){
 	var gameTime = TOTAL_TIME;  //set to max time of game, in seconds
 	var	prev_time = getTimeInSec(); //time of previous tick
 	var score = 0;
-	var energy = MAX_ENERGY;
+	//var energy = Math.random()*MAX_ENERGY/2 + MAX_ENERGY/50;
+	var energy = 100;
 	var energy_multiplier = 1.0; //At each second, energy drops this amount
 	var energy_dist_multiplier = 0.05; //for calculating energy loss from travel distance
 
@@ -46,18 +47,52 @@ function init(){
 	};
 
     //ADDING SHAPES
+    var rand_x = get_random_location();
+    var rand_y = rand_x[1];
+    var rand_x = rand_x[0];
 
-    var red1 = createCircle("red", 25, 400, 250, 0,0, false);
-    var red2 = createCircle("red", 25, 100, 100, 0.0, false);
+    var red1 = createRandomCircle("red", null, 0.0, false);
+    var red2 = createRandomCircle("red", 25, 0.0, false);
+    var red3 = createRandomCircle("red", null, 0.0, false);
+    var red4 = createRandomCircle("red", null, 0.0, false);
+    var red5 = createRandomCircle("red", 25, 0.0, false);
+    var red6 = createRandomCircle("red", null, 0.0, false);
+    var red7 = createRandomCircle("red", null, 0.0, false);
+    var red8 = createRandomCircle("red", 25, 0.0, false);
+    var red9 = createRandomCircle("red", null, 0.0, false);
     
-    var blk1 = createCircle("black", 25, 200, 300, 0.0, true);
-    var blk2 = createCircle("black", 25, 350, 100, 0.0, true);
+    var blk1 = createRandomCircle("black", 25, 0.0, true);
+    var blk2 = createRandomCircle("black", null, 0.0, true);
+    var blk3 = createRandomCircle("black", null, 0.0, true);
+    var blk4 = createRandomCircle("black", null, 0.0, true);
+    var blk5 = createRandomCircle("black", 25, 0.0, true);
+    var blk6 = createRandomCircle("black", 25, 0.0, true);
+    var blk7 = createRandomCircle("black", null, 0.0, true);
+    var blk8 = createRandomCircle("black", null, 0.0, true);
+    var blk9 = createRandomCircle("black", null, 0.0, true);
+    var blk10 = createRandomCircle("black", 25, 0.0, true);
+ 
 
     shapes = {
     	r1 : red1,
     	r2 : red2,
+    	r3 : red3,
+    	r4 : red4,
+    	r5 : red5,
+    	r6 : red6,
+    	r7 : red7,
+    	r8 : red8,
+    	r9 : red9,
     	b1 : blk1,
-    	b2 : blk2
+    	b2 : blk2,
+    	b3 : blk3,
+    	b4 : blk4,
+    	b5 : blk5,
+    	b6 : blk6,
+    	b7 : blk7,
+    	b8 : blk8,
+    	b9 : blk9,
+    	b10 : blk10
     };
     // console.log(shapes[1]);
 
@@ -74,12 +109,14 @@ function init(){
 	var main = createCircle("blue", 30, 40, stage.canvas.height/2);
 	stage.addChild(main);
 
-	//main follows mouse
-	// stage.on("stagemousemove",function(evt){
-	// 	main.x = evt.stageX;
-	// 	main.y = evt.stageY;
-	// 	stage.update();
-	// });
+	//end sprite
+	var bed = createCircle("green", 30, stage.canvas.width-40, stage.canvas.height/2);
+	var bed_container = new createjs.Container();
+	bed_container.addChild(bed);
+	bed_container.on("click", function(evt){
+		console.log("GAME OVER!");
+	});
+	stage.addChild(bed);
 
     stage.update();
 
@@ -149,33 +186,56 @@ function init(){
 		console.log(tiredness);
 		return dist * energy_dist_multiplier*tiredness;
 	}
-};
 
-/**
-* Create filled circle with given color, size, at given location
-* color: string with circle color ("red", "black")
-* size: int for radius of circle
-* xPos, yPos: x and y coordinates relative to upper left corner of canvas
-* isProductive: True if dot is productive (gets you score)
-*/
-function createCircle(color, size, xPos, yPos, timeAlive, isProductive){
-	var circle = new createjs.Shape();
+	//gets location within canvas
+	function get_random_location(){
+		var padding = 50; //stay at least 50 px away from each edge
+		x = Math.round(Math.random()*(stage.canvas.width-padding*2)+padding);
+		y = Math.round(Math.random()*(stage.canvas.height-padding*2)+padding);
+		console.log ([x,y]);
+		return [x,y]
 
-	//default args
-	size = size || 50;
-	xPos = xPos || 100;
-	yPos = yPos || 100;
-	timeAlive = timeAlive || 5.0;
+	};
+	/**
+	* Create filled circle with given color, size, at given location
+	* color: string with circle color ("red", "black")
+	* size: int for radius of circle
+	* xPos, yPos: x and y coordinates relative to upper left corner of canvas
+	* isProductive: True if dot is productive (gets you score)
+	*/
+	function createCircle(color, size, xPos, yPos, timeAlive, isProductive){
+		var circle = new createjs.Shape();
 
-	circle.graphics.beginFill(color).drawCircle(0,0,size);
-	circle.x = xPos;
-	circle.y = yPos;
-	circle.timeAlive = 0.0; //TODO: Change for real game
+		//default args
+		size = size || 50;
+		xPos = xPos || 100;
+		yPos = yPos || 100;
+		timeAlive = timeAlive || 5.0;
 
-	if(isProductive){ circle.isWork=true; }
-	else{ circle.isWork = false; }
-	
-	return circle;
+		circle.graphics.beginFill(color).drawCircle(0,0,size);
+		circle.x = xPos;
+		circle.y = yPos;
+		circle.timeAlive = 0.0; //TODO: Change for real game
+
+		if(isProductive){ circle.isWork=true; }
+		else{ circle.isWork = false; }
+		
+		return circle;
+	};
+
+	//create circle at random location (with random size?);
+	function createRandomCircle(color, size, timeAlive, isProductive){
+		var max_size = 80; //max bubble size
+		var min_size = 10;
+		//default args
+		size = size || Math.round(Math.random()*(max_size-min_size)+min_size); //size between 10 and 100
+		timeAlive = timeAlive || 5.0;
+
+		rand_coord = get_random_location();
+		x = rand_coord[0];
+		y = rand_coord[1];
+		return createCircle(color, size, x, y, timeAlive, isProductive);
+	}
 };
 
 function getTimeInSec(){
